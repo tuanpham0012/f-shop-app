7ba6c0b6becdf0cb0bf90dfb0893b2e07e25e070
Phạm Quốc Tuấn <tuanpham0012@gmail.com>
2024-10-24 10:16:59 +0700

    update

--- client/src/helpers/config.ts
+++ client/src/helpers/config.ts
@@ -3,16 +3,16 @@ export let apiUrl = "";
 const env = import.meta.env.VITE_VUE_APP_ENV;
 console.log(import.meta.env.VITE_VUE_APP_ENV);
 
-console.log(window.location.host);
+console.log();
 
 switch (env) {
     case "production":
         apiUrl = `https://${import.meta.env.VITE_VUE_APP_DOMAIN_PROD}/api/v1`;
         break;
     case "development":
-        apiUrl = `:8080/api/v1`;
+        apiUrl = `${window.location.protocol}//${window.location.hostname}:8080/api/v1`;
         break;
     default:
-        apiUrl = "http://localhost:5077/api/v1";
+        apiUrl = `${window.location.protocol}//${window.location.hostname}:5077/api/v1`;
         break;
 }
