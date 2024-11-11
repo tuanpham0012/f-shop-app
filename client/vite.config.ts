8e92d90272f9213cf82fa30f89eba9f3a4cfbf13
Phạm Quốc Tuấn <tuanpham0012@gmail.com>
2024-11-11 23:07:13 +0700

    update --res

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -7,7 +7,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: './',
+  base: '/',
   plugins: [
     vue(),
     vueJsx(),
