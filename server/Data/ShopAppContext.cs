edf09806e406ddffb4c2d1d6ccd87a4e4c59ab10
Phạm Quốc Tuấn <tuanpham0012@gmail.com>
2026-02-27 09:03:58 +0700

    test CICD

--- server/Data/ShopAppContext.cs
+++ server/Data/ShopAppContext.cs
@@ -71,9 +71,9 @@ public partial class ShopAppContext : DbContext
 
     public virtual DbSet<Warehouse> Warehouses { get; set; }
 
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Server=localhost,1434;Database=shop_app;Trust Server Certificate=True;User ID=sa;Password=Anhem123@; Max Pool Size=20000;Connection Timeout=13000;");
+//     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+// #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+//         => optionsBuilder.UseSqlServer("Server=localhost,1434;Database=shop_app;Trust Server Certificate=True;User ID=sa;Password=Anhem123@; Max Pool Size=20000;Connection Timeout=13000;");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
